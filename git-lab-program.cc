f690246fef5fe541ec2607e105e1503cb555a15f
Lucas Fernandes <lf508319@ohio.edu>
2021-01-26 19:29:41 -0500

    ADDING GIT-LAB-PROGRAM

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Lucas Fernandes
+ *        Date: 26th of January 2021
+ * Description: git-lab-program.cc
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
